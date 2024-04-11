eb4def616946e6bbbd07c570b00a62e3a0fcb53c
Bunthanhdam <76465481+buithanhdam@users.noreply.github.com>
2024-04-11 15:30:28 +0700

    first commit

---
+++ src/main/java/vn/edu/hcmuaf/fit/employeeexpense/EmployeeExpenseApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.fit.employeeexpense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeExpenseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeExpenseApplication.class, args);
+    }
+
+}
